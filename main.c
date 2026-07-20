1ae11e31e61adb451ec3bd32363f89139fac48b5
YOGES-2097 <yogeswaranm2907@gmail.com>
2026-07-20 23:54:29 +0530

    add servo

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
+#inclued<servo.h>
+
 void main(){
 	pinMode(13,OUTPUT);
+	pinMode(9,OUTPUT);
 }
 void loop(){
 	digitalWrite(13,HIGH);
 	delay(1000);
 	digitalWrite(13,LOW);
-	delay(500);}
+	delay(500);
+	
+}
