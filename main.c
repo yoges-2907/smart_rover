3300cc09b5f478cf6e14799f1fe5ba5aeac42242
YOGES-2097 <yogeswaranm2907@gmail.com>
2026-07-22 21:45:46 +0530

    full motor control

--- main.c
+++ main.c
@@ -1,13 +1,148 @@
-#inclued<servo.h>
+#include <ESP32Servo.h>
 
-void main(){
-	pinMode(13,OUTPUT);
-	pinMode(9,OUTPUT);
+#define ENA 5
+#define IN1 6
+#define IN2 7
+
+#define ENB 8
+#define IN3 9
+#define IN4 10
+
+#define TRIG 3
+#define ECHO 4
+
+Servo radarServo;
+
+long distance();
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+
+    radarServo.attach(2);
+
+    analogWrite(ENA, 180);
+    analogWrite(ENB, 180);
 }
-void loop(){
-	digitalWrite(13,HIGH);
-	delay(1000);
-	digitalWrite(13,LOW);
-	delay(500);
-	
+
+void loop()
+{
+    long d = distance();
+
+    Serial.println(d);
+
+    if (d > 25)
+    {
+        forward();
+    }
+    else
+    {
+        stopMotor();
+
+        delay(300);
+
+        backward();
+
+        delay(400);
+
+        stopMotor();
+
+        radarServo.write(20);
+
+        delay(500);
+
+        long left = distance();
+
+        radarServo.write(160);
+
+        delay(500);
+
+        long right = distance();
+
+        radarServo.write(90);
+
+        if (left > right)
+        {
+            turnLeft();
+        }
+        else
+        {
+            turnRight();
+        }
+
+        delay(500);
+
+        stopMotor();
+    }
+}
+
+long distance()
+{
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(TRIG, LOW);
+
+    long duration = pulseIn(ECHO, HIGH);
+
+    return duration * 0.034 / 2;
+}
+
+void forward()
+{
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void backward()
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+}
+
+void turnLeft()
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void turnRight()
+{
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+}
+
+void stopMotor()
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
 }
